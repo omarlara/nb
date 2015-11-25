af95f38e0d63ad7b51c91ca1d41533b6829e4ad1
Marco Antonio Dmz <madmzr@gmail.com>
2015-11-24 19:39:40 -0600

    Added states on Results components

--- js/app.js
+++ js/app.js
@@ -96,6 +96,65 @@
 
                     $element.toggleClass('opened closed');
                 });
+
+            $element.find('.submiter')
+                .bind('click', function (e) {
+                    e.preventDefault();
+
+                    var currentCode = $element.find('name=[zip-tool]').val(),
+                        success = 'Your new address is serviced by Enbridge.',
+                        error = 'Your new address is not serviced by Enbridge. Please contact your local municipality to find a local provicer. You may still proceed in order to let us know when you are moving out.',
+                        request = null,
+                        result = null;
+
+                    request =  $.post( "http://beta.json-generator.com/api/json/get/4yzCsvG1x",
+                                    function(resp) {
+                                        if (resp && resp.result) {
+                                            $element
+                                                .find('.result')
+                                                    .html('<img src="assets/img/success.png"> <span>' + success + '</span>')
+                                                    .addClass('success-code');
+                                        } else {
+                                            $element
+                                                .find('.result')
+                                                    .html('<img src="assets/img/error.png"> <span>' + error + '</span>')
+                                                    .addClass('error-code');
+                                        }
+
+                                },"json");
+                    /*
+                    $.ajax({
+                        method: "POST",
+                        url: "http://beta.json-generator.com/api/json/get/4yzCsvG1x",
+                        data: {
+                            value: currentCode
+                        },
+                        statusCode: {
+                            200: function() {
+                                console.log('Yeeee');
+                            },
+                            404: function() {
+                                console.log( "page not found" );
+                            }
+                        }
+                    })
+                    .done(function(resp) {
+                        if (!!resp.result) {
+                            $element
+                                .find('.result')
+                                    .text(success)
+                                    .addClass('success-code');
+                        } else {
+
+                        }
+                    })
+                    .fail(function() {
+                        console.log('fail');
+                    });
+                    */
+                /**/
+
+                });
         }
 
         $.fn.codeTool = function (element) {

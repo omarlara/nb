7e5d73c75db3a7198d425d2c08f07d241b781f94
Marco Antonio Dmz <madmzr@gmail.com>
2015-11-26 23:45:51 -0600

    Third step

--- js/app.js
+++ js/app.js
@@ -307,6 +307,53 @@
             }
         });
 
+        $('#get-adreess').bind('click', function() {
+            var $this = $(this);
+
+            $.post( "http://beta.json-generator.com/api/json/get/4yzCsvG1x",
+                function(resp) {
+                    if (resp && resp.result) {
+                        var numbers = [],
+                            container = $this.attr('data-number-dropdown');
+
+                        $(container).html('');
+
+                        for(var i = 0, size = resp.numbers.length; i < size; i++) {
+                            numbers.push('<option value="' + resp.numbers[i] + ' ">' + resp.numbers[i] + '</option>')
+                        }
+
+                        $('<select class="enbridge-select" id="current-adress">' + numbers.join('') + '</select>')
+                            .appendTo(container)
+                            .enbridgeDropdown();
+
+                        /*var containerEl = container.replace('#','');
+                        $this
+                            .addClass('success-field')
+                            .removeClass('input-error');
+
+                        for(var i = 0, size = resp.elements.length; i< size; i +=1) {
+                            var chain = '<input type="radio" id="' + (containerEl + '-' + i) + '"  name="' + containerEl + '"value="' + resp.elements[i].value + '" name="stepsContent"><label class="fake-input" for="' + (containerEl + '-' + i) + '">' + resp.elements[i].name + '</label>';
+                            radioContent.push(chain);
+                        }
+
+                        $(container).html(radioContent.join(''));
+
+                         $(container).find('input[type="radio"]').bind('click', function() {
+                            var name = $(this).attr('name') || '';
+
+                            $('input[name="' + name + '"]').removeClass('input-success input-error');
+                        });*/
+
+
+                    } else {
+                        $this
+                            .removeClass('input-success success-field')
+                            .addClass('input-error');
+                    }
+
+                },"json");
+        });
+
         /*End Flow*/
 
 

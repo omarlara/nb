aea8c069851d578b7b57c9db96c6a9cc486b9593
jesus ibanez <ashernetz@hotmail.com>
2015-12-02 16:08:08 -0600

    tiles

---
+++ js/dialog.js
@@ -0,0 +1,25 @@
+function opendialog(e)
+{
+	switch(e.id)
+	{
+		case 'green':
+			var dialog = "#movegreen";
+		break;
+	}
+
+	$(function(){ 
+        var  button = $("body");
+        $(dialog).dialog({
+            resizable: false,
+            height:240,
+            width:720,
+            modal: true,
+            height:440
+        });
+        
+        
+        
+        
+    });
+	
+}

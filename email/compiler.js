cd0cc22cbf7930e828354ceb057543b4e9fc4b5d
Manuel Escobedo <bigmander@gmail.com>
2016-02-26 16:51:16 -0600

    Draft of compiler

--- email/compiler.js
+++ email/compiler.js
@@ -1,9 +1,23 @@
-var handlebars = require('handlebars');
 var fs = require('fs');
+var handlebars = require('handlebars');
+var files = fs.readdirSync('partials');
 
-var compileTemplate = process.argv[2];
-var contentTemplate = fs.readFileSync(compileTemplate, 'utf-8');
+var partialRoute = 'partials/';
 
-var template = handlebars.compile(contentTemplate);
-var html = template({ hola: 'hola!' });
+var partialName = '';
+var content = '';
 
+files.forEach(function (f) {
+    partialName = f.replace(/\.hbs$/, '');
+    content = fs.readFileSync(partialRoute + f, 'utf-8');
+    handlebars.registerPartial(partialName, content);
+});
+
+var templateFile = process.argv[2];
+var source = fs.readFileSync(templateFile, 'utf-8');
+
+var settings = process.argv[3] || 'data.json';
+var template = handlebars.compile(source);
+var context = JSON.parse(fs.readFileSync(settings, 'utf8'));
+var html = template(context);
+console.log(html);
